a45a3982bd8367061c074c034b4d3cb8c5bd1521
Mateus <mateusfpleite@gmail.com>
2022-03-07 18:22:19 -0300

    adiciona exercicios 4.1 onde tive que construir programas para uma adição, subtração, multiplicação, divisão e módulo

---
+++ fundamentos/bloco-04-introducao-a-javascript/exercicio4.1.3.js
@@ -0,0 +1,4 @@
+const a = 10;
+const b = 3; 
+resultadoMult = a*b;
+console.log(resultadoMult)
--- fundamentos/bloco-04-introducao-a-javascript/script.js
+++ fundamentos/bloco-04-introducao-a-javascript/script.js
@@ -4,3 +4,4 @@ let birthYear = 1997;
 console.log(myName, birthCity, birthYear)
 birthYear = 2030;
 console.log(birthYear);
+//
