c8dddea8bc078467c487a41aa815f4db98088c8e
Mateus <mateusfpleite@gmail.com>
2022-03-07 18:09:50 -0300

    adiciona exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript/script.js
@@ -0,0 +1,6 @@
+const myName = "Mateus";
+const birthCity = "Belo Horizonte";
+let birthYear = 1997;
+console.log(myName, birthCity, birthYear)
+birthYear = 2030;
+console.log(birthYear);
---
+++ fundamentos/bloco-04-introducao-a-javascript/script2.js
@@ -0,0 +1,6 @@
+const base = 5;
+const height = 8;
+const area = base*height;
+console.log(area);
+const perimeter = 2*(base+height);
+console.log (perimeter);
